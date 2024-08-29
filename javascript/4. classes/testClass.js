cc97f0eb2e1d613c9d656d91e0136c4c44682296
AdeilH <madeel2344@gmail.com>
2024-08-29 19:04:13 +0000

    Object and class

---
+++ javascript/4. classes/testClass.js
@@ -0,0 +1,15 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+class Struggle{
+    constructor(level, intensity){
+        this.level = level;
+        this.intensity = intensity;
+    }
+
+    get totalStruggle() {
+        return this.level * this.intensity;
+    }
+};
+
+let struggle = new Struggle(10, 10);
+console.log(struggle.totalStruggle);

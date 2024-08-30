ddf3384e9b5af88806fe3df66ca2d01d7a5aaa94
AdeilH <madeel2344@gmail.com>
2024-08-30 18:27:10 +0000

    Some more js

--- javascript/2. variables/variables.js
+++ javascript/2. variables/variables.js
@@ -14,3 +14,37 @@ console.log(testBoolean);
 console.log(testDouble);
 console.log(undefined);
 console.log(nullObject);
+
+let price = 1_000_000;
+let americanFormat = price.toLocaleString("en-US");
+let punjabFormat = price.toLocaleString("pa-PA");
+console.log(americanFormat);
+console.log(punjabFormat);
+
+
+let currencyformatter = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"});
+let currencyformatter2 = new Intl.NumberFormat("pa-PA", {style: "currency", currency: "JPY"});
+
+console.log(`${currencyformatter2.format(price)}`);
+
+// Dates
+
+var date = new Date();
+
+console.log(date.toDateString());
+
+console.log(date.toLocaleDateString("en-US"));
+console.log(date.toLocaleDateString("en-UK"));
+console.log(date.toLocaleDateString("ja-JP"));
+console.log(date.toLocaleDateString("ur-PK"));
+
+
+console.log(date.toLocaleTimeString("en-US"));
+console.log(date.toLocaleTimeString("en-UK"));
+
+let options = {
+    dateStyle: "long",
+    timeStyle: "long"
+};
+
+console.log(date.toLocaleString("en-US", options));

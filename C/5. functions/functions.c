6f341ca9f5e821fc4c15b7c341eb25f4d3f6b79b
AdeilH <madeel2344@gmail.com>
2024-09-01 18:51:54 +0000

    generic swap function

--- C/5. functions/functions.c
+++ C/5. functions/functions.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <string.h> 
+#include <stdlib.h> 
+// generic swap function
+
+void swap(void *a, void *b, int size){
+    void * tmp;
+    tmp = malloc(size);
+    memcpy(tmp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tmp, size);
+    free(tmp);
+    return; 
+}
 
 long long recursionFactorial(long n){
     if (n < 0){
@@ -30,5 +43,13 @@ unsigned long long tailRecursionFactorial(long n, long a){
 int main(){
     printf("%lld \n", recursionFactorial(20));
     printf("%lld \n", tailRecursionFactorial(20, 1));
+    int a = 10;
+    int b = 20; 
+    printf("a = %d, b = %d \n", a, b);
+    swap((void *)&a, (void *)&b, sizeof(int));
+    printf("a = %d, b = %d \n", a, b);
+
     return 0;
+
+
 }
